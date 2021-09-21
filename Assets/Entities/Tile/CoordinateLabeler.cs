72f0093e18b6e9603332c43835330045eb0216e0
Stedd <tor.andre.paulsen@gmail.com>
2021-09-21 09:44:11 +0200

    Added coordinate labeler +Folder organizations

---
+++ Assets/Entities/Tile/CoordinateLabeler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+TextMeshPro label;
+Vector2Int coord = new Vector2Int();
+
+private void Awake() {
+    label=GetComponent<TextMeshPro>();
+    UpdateLabelName();
+}
+
+private void Update() {
+    UpdateLabelName();
+    UpdateObjectName();
+}
+
+void UpdateLabelName(){
+    coord.x = Mathf.RoundToInt(transform.parent.position.x/UnityEditor.EditorSnapSettings.move.x);
+    coord.y = Mathf.RoundToInt(transform.parent.position.z/UnityEditor.EditorSnapSettings.move.z);
+
+    label.text = $"{coord.x},{coord.y}";
+}
+
+void UpdateObjectName(){
+    transform.parent.name = coord.ToString();
+}
+
+}
