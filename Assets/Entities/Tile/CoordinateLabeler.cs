3a816dadf0a200b18f124f4a4e576b30a76cd879
Stedd <tor.andre.paulsen@gmail.com>
2021-10-02 13:53:58 +0200

    Toggleable tile debug labels

--- Assets/Entities/Tile/CoordinateLabeler.cs
+++ Assets/Entities/Tile/CoordinateLabeler.cs
@@ -15,23 +15,43 @@ Vector2Int coord = new Vector2Int();
 private void Awake() {
     tile    = GetComponentInParent<Tile>();
     label   = GetComponent<TextMeshPro>();
+    label.enabled = false;
     UpdateLabelName();
 }
 
 private void Update() {
-    UpdateLabelName();
-    UpdateObjectName();
+
+    ToggleLabels();
+    
+    if(label.enabled){
+        UpdateLabelColor();
+    }
+    
+    if(!Application.isPlaying){
+        label.enabled = true;
+        UpdateLabelName();
+        UpdateObjectName();
+    }
+}
+
+void ToggleLabels(){
+    if(Input.GetKeyDown(KeyCode.L)){
+        label.enabled = !label.enabled;
+    }
 }
 
 void UpdateLabelName(){
     coord.x = Mathf.RoundToInt(transform.parent.position.x/UnityEditor.EditorSnapSettings.move.x);
     coord.y = Mathf.RoundToInt(transform.parent.position.z/UnityEditor.EditorSnapSettings.move.z);
 
+    label.text = $"{coord}";
+}
+
+void UpdateLabelColor(){
     label.color = defaultColor;
     if(!tile.IsPlaceable){
         label.color = occupiedColor;
     }
-    label.text = $"{coord}";
 }
 
 void UpdateObjectName(){
